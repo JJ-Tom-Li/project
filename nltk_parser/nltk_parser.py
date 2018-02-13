24e6781d357e32abac4080ccaa64162926dfef5f
pandatomlcc <pandatomlcc@users.noreply.github.com>
2018-02-13 22:52:36 +0800

    Add files via upload

--- nltk_parser/nltk_parser.py
+++ nltk_parser/nltk_parser.py
@@ -5,11 +5,10 @@ class NLTK_parser:
 	#count the frequence
 	fdist = FreqDist()
 	def __init__(self):
-		self.stopworddic.update([',','.','(',')','\'',':','The','\'\'','``','\'s'])
+		self.stopworddic.update([',','.','(',')','\'',':','\'\'','``','\'s'])
 		self.fdist = FreqDist()
 		
 	def parser(self,dir,outfile,number_of_result):
-		sentence = []		#存切出來的詞
 		file_number = 1		#計算分析到第幾個檔案
 		for filename in os.listdir(dir):
 			#If file is not end by ".txt" then continue
@@ -26,8 +25,8 @@ class NLTK_parser:
 			#如果沒有指定輸出檔名 直接顯示在螢幕上
 			print("出現頻率前 "+str(number_of_result)+" 名：")
 			for word,index in zip(nfdist,range(0,number_of_result)):
-				if(index==20):
-					#如果顯示二十個就結束
+				if(index==number_of_result):
+					#如果顯示到目標數量就結束
 					break
 				#輸出至螢幕
 				print("\t"+word+" : "+str(self.fdist[word]))
@@ -35,8 +34,8 @@ class NLTK_parser:
 			with open(outfile,"w") as outf:	
 				outf.write("出現頻率前 "+str(number_of_result)+" 名：\n")
 				for word,index in zip(nfdist,range(0,number_of_result)):
-					if(index==20):
-						#如果顯示二十個就結束
+					if(index==number_of_result):
+						#如果顯示到目標數量就結束
 						break
 					#輸出至檔暗
 					outf.write("\t"+word+" : "+str(self.fdist[word])+"\n")
@@ -46,7 +45,7 @@ class NLTK_parser:
 					#讀入檔案
 					SampleTXT = str(f.read())
 					#去掉\r\n\r\n
-					paragraphs = [p for p in SampleTXT.split('\\r\\n\\r\\n') if p]
+					paragraphs = [p.lower() for p in SampleTXT.split('\\r\\n\\r\\n') if p]
 					#斷字
 					sentence=[]
 					for paragraph in paragraphs:

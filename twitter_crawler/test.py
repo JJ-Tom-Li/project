3e58cef6d7f30dfa7e85da5e100bdd838189ae92
pandatomlcc <pandatomlcc@users.noreply.github.com>
2018-03-15 01:56:50 +0800

    Add files via upload

--- twitter_crawler/test.py
+++ twitter_crawler/test.py
@@ -1,12 +1,49 @@
 from twitter_crawler import Twitter_crawler
 import pickle
-with open("keys.txt","r") as f:
+import json
+with open("keys/keys2.txt","r") as f:
 	tmplist = f.read().split('\n')
 	consumer_key = tmplist[0] 
 	consumer_secret = tmplist[1]  
 	access_token = tmplist[2]  
 	access_token_secret = tmplist[3]
+	
 tc = Twitter_crawler(consumer_key,consumer_secret,access_token,access_token_secret)
+
+with open("experts/horus_experts.txt","r") as inputf:
+	try:
+		names = []
+		old_data = []
+		with open("experts/horus_experts.json",encoding='utf-8') as jsonf:
+			try:
+				old_data = json.loads(jsonf.read())
+				for expert in old_data:
+					names.append(expert['screen_name'])
+			except json.decoder.JSONDecodeError :
+				print("Json file error.")
+	except FileNotFoundError:
+		print("Json file is not found.Create new one.")
+			
+	with open("experts/horus_experts.json","w") as jsonf:
+		outputjson=[]
+		if len(names)!=0:
+			for name in names:
+				print(name)
+			outputjson.extend(old_data)
+		experts_name = inputf.read().split('\n')
+		for expert in experts_name:
+			if expert == "" :
+				#blank name
+				continue
+			if expert in names:
+				print(expert+" is already in file.")
+				continue
+			user = tc.get_user(expert)
+			outputjson.append(user)
+			print("Expert \""+expert+"\" done.")
+		jsonf.write(json.dumps(outputjson))
+		
 #tc.crawler("output.json","CVEnew",1,1)
 #tc.print_json("output.json")
-tc.get_followers("CVEnew")
+#tc.get_followers("Cvenew")
+#tc.search()
--- twitter_crawler/twitter_crawler.py
+++ twitter_crawler/twitter_crawler.py
@@ -2,7 +2,7 @@ import tweepy
 import json
 import time
 class Twitter_crawler:
-	def __init__(self,consumer_key,consumer_secret,access_token,access_token_secret):
+	def __init__(self,consumer_key,consumer_secret,access_token,access_token_secret,a=1):
 		self.consumer_key=consumer_key
 		self.consumer_secret=consumer_secret
 		self.access_token=access_token
@@ -30,8 +30,23 @@ class Twitter_crawler:
 					dataout.append(j)
 				print("Page " + str(i) + " finished.")
 			jsonf.write(json.dumps(dataout))
-			
-	def print_json(self,filename):
+	def crawler_by_time(self,outputjson,twitter_name,start_time,end_time):
+		# Twitter target
+		name = twitter_name
+		# number of tweets
+		number=1;
+		dataout=[]
+		# crawl	
+		print("@"+name+":start crawl:")
+		with open(outputjson,"w") as jsonf:
+			for i in range(start_page,max_page+1):
+				# user user_timeline() to crawl tweet
+				results = self.api.user_timeline(id=name, page=i,tweet_mode='extended')
+				for index,j in enumerate(results):
+					dataout.append(j)
+				print("Page " + str(i) + " finished.")
+			jsonf.write(json.dumps(dataout))
+	def print_cve_json(self,filename):
 		inputdata=[]
 		with open(filename, encoding='utf-8') as f:
 			inputdata=json.loads(f.read())
@@ -45,27 +60,58 @@ class Twitter_crawler:
 				
 				print("\tCreated at:"+i['created_at'] )
 				print("\tURL:"+i['entities']['urls'][0]['expanded_url']+"\n")
+	def get_user(self,username):
+		user = self.api.get_user(username)
+		return user
+	def user_to_json(self,user):
+		j = json.dumps(user,ensure_ascii=False)
+		return j
+	'''	
 	def get_followers(self,twitter_name):
 		follower_id = []
 		dataout=[]
 		index=0
 		api = tweepy.API(self.auth, wait_on_rate_limit=True
 						 , wait_on_rate_limit_notify=True, compression=True)
-		for page in tweepy.Cursor(api.followers_ids,screen_name=twitter_name).pages():
-			#print(page)
-			index = index + 1
-			follower_id.extend(page)
-			#等待下一頁
-			#time.sleep(10)
-	#	user = self.api.get_user(twitter_name)
-	#	print(user['followers_count'])
-		with open("follower","w") as f:
+
+		api = tweepy.API(self.auth, compression=True)
+		real_number = 0
+
+		for page in tweepy.Cursor(api.followers,screen_name=twitter_name).pages():
+
+				index = index + 1
+				real_number  = real_number + len(page)
+				for item in page:
+					if(item.followers_count<1000):
+						page.remove(item)
+				follower_id.extend(page)
+				print("real:"+str(real_number))
+				print(len(follower_id))
+			for follower in follower_id:
+				print(follower)
+
+		with open("followers2","w") as f:
 			index = 0
+			key = 1
 			for uid in follower_id:
 				index = index + 1
-			#	if index == 2:
-				#	break
-				user = api.get_user(uid)
-				print(str(index)+":"+user.screen_name)
-				#print(uid)
-				f.write(user.screen_name+"\n")
+				try:
+					user = api.get_user(uid)
+					if(user.followers_count>=100):
+						print(str(index)+":"+user.screen_name)
+					f.write(user.screen_name+"\n")
+				except:
+					print("change keys")
+					key = key + 1
+					filename = "keys/keys"+str(key)+".txt"
+					self.get_keys(filename)
+					api = tweepy.API(self.auth, compression=True)
+	'''
+	def get_keys(self,filename):
+		with open(filename,"r") as f:
+			tmplist = f.read().split('\n')
+			consumer_key = tmplist[0] 
+			consumer_secret = tmplist[1]  
+			access_token = tmplist[2]  
+			access_token_secret = tmplist[3]
+		self.__init__(consumer_key,consumer_secret,access_token,access_token_secret)

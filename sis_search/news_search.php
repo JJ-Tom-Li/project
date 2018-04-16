7e873ba679f07ad874284ffcee6a175e8418aa21
pandatomlcc <pandatomlcc@users.noreply.github.com>
2018-04-16 22:52:13 +0800

    Add files via upload

--- sis_search/news_search.php
+++ sis_search/news_search.php
@@ -5,25 +5,30 @@
     }
 
     function search_by_author($con,$author_keyword){
+        //First get the list of author ids.
         $author_id_list = get_author_id_from_author_name($con,$author_keyword);
+
+        //Turn list into string array.
         $id_list = array();
         for ($i=0;$i<mysqli_num_rows($author_id_list);$i++){
             array_push($id_list,mysqli_fetch_row($author_id_list)[0]);
         }
-        /*for ($i=0;$i<mysqli_num_rows($author_id_list);$i++){
-            echo $id_list[$i];
-            echo '<br>';
-            
-        }*/
-     //   $id_list = implode(',', $id_list);
+
         return mysqli_query($con,"select * from news_header where author_id in (".implode(",",$id_list).")");
     }
 
     function search_by_body($con,$body_keyword){
-        return mysqli_query($con,"select * from news_header where news_content like '%$body_keyword%'");
+        $news_list = mysqli_query($con,"select news_id from news_content where news_body like '%$body_keyword%'");
+        
+        //Turn list into string array.
+        $id_list = array();
+        for ($i=0;$i<mysqli_num_rows($news_list);$i++){
+            array_push($id_list,mysqli_fetch_row($news_list)[0]);
+        }
+        return mysqli_query($con,"select * from news_header where news_id in (".implode(",",$id_list).")");
     }
 
     function search_by_source($con,$source_keyword){
-        return mysqli_query($con,"select * from news_header where title like '%$title_keyword%'");
+        return mysqli_query($con,"select * from news_header where news_source like '%$source_keyword%'");
     }
 ?>
